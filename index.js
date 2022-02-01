a5caffccaea4c401dbe2f59fbec5d8756ea290ac
av3n <ilyapech01@gmail.com>
2022-02-01 18:38:48 +0400

    Finally

--- index.js
+++ index.js
@@ -1,10 +1,49 @@
 function initPage() {
 
-    document.getElementById("button_load").onclick = () => {
-        document.getElementById("img_src").src = null;
-    };
+    let state = {
+        rover: "",
+        sol: 0
+    }
+
+    document.getElementById("rover-selector").addEventListener("change", (event) => {
+        state.rover = event.target.value;
+    });
+
+    document.getElementById("sol-input").addEventListener("input", (event) => {
+        state.sol = event.target.value;
+    });
+
+    document.getElementById("button_load").addEventListener("click", (event) => {
+        const photosBlock = document.getElementById("photos_block");
+        Array.from(photosBlock.children).forEach((el) => {el.remove();});
+        console.log(state);
+        loadImage(state.rover, state.sol)
+        .then((value) => {
+            const {photos} = value;
+            
+            for (const photo of photos) {
+                let img = document.createElement("img");
+                img.alt = "Фото с марса";
+                img.src = photo.img_src;
+                photosBlock.appendChild(img);
+            }
+        })
+        .catch((error) => {
+            console.warn(error.message);
+        });
+        event.preventDefault();
+    });
 }
 
-function loadImage(rover, sol) {
-    fetch(`https://api.nasa.gov/mars-photos/api/v1/${rover}/curiosity/photos?sol=${sol}&api_key=DEMO_KEY`)
+async function loadImage(rover, sol) {
+    const responce = await fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/${rover}/photos?sol=${sol}&api_key=DEMO_KEY`);
+
+    if (responce.status === 200) {
+        return responce.json();
+    } else {
+        throw new Error("Не удалось загрущить картинку!");
+    }
+    return {photos:[]};
 }
+
+initPage();
